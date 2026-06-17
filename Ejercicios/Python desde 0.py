b930fc505064003575a1f04e343a370180c9d81e
Xakir89 <xakir89@gmail.com>
2026-06-16 21:54:48 -0500

    Create Python desde 0.py

---
+++ Ejercicios/Python desde 0.py
@@ -0,0 +1,44 @@
+print("\n.......Suma de dos Numeros.......\n")
+numero_uno = 3
+Numero_uno = 2
+resultado = numero_uno + Numero_uno
+print("Este es el Resultado = ",resultado,"\n")
+
+print(".......Concatenacion de Cadenas.......\n")
+mensaje = "Hola "
+espacio = " "
+nombre = "Ernesto"
+mensaje = mensaje + espacio + nombre
+print("\n",mensaje,"\n")
+
+print(".......buscar Subcadena .find.......\n")
+mensaje = "Hola querido Ernesto"
+buscar_subcadena = mensaje.find("querido")
+print("La subcadena se encuentra en la posicion = ",buscar_subcadena,"\n")
+
+mensaje = "Hola "
+mensaje += "querido "
+mensaje += "Ernesto"
+print("\n",mensaje,"\n")
+
+print(".......Asignacion de Variables.......\n")
+Numero_uno = 8
+Numero_dos = 20
+resultado = Numero_uno + Numero_dos
+print("El resultado de la suma es = ",resultado,"\n")
+
+print(".......extraer Subcadena.......\n")
+mensaje = "Hola querido Ernesto"
+subcadena = mensaje[5:12]
+print("La subcadena es = ",subcadena,"\n")
+
+print(".......Comparacion de Cadenas.......\n")
+cadena1 = "Hola"
+cadena2 = "Hola"
+resultado = cadena1 == cadena2
+print("El resultado de la comparacion es = ",resultado,"\n")
+
+print(".......Buscar` una Subcadena.......\n")
+mensaje = "Hola querido Ernesto"
+subcadena = mensaje[5:12]
+print("La subcadena es = ",subcadena,"\n")
